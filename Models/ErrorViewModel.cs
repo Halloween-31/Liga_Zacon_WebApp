9b2a9fa0207a711dc0a13defb51a57f20c25126a
FireM <FireM@LAPTOP-52VB6LHQ>
2026-03-16 19:13:45 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Liga_Zacon_WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
